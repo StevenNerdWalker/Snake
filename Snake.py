19d6a757bdb7ffded2c08ee72585a9bb71014988
StevenNerdWalker <lucasamvencio@gmail.com>
2022-12-20 10:39:30 -0300

    I dont know how to do this
    I want to get input at any time, but if I call getch it wont run anything else until it gets some input

--- Snake.py
+++ Snake.py
@@ -1,9 +1,35 @@
 import msvcrt as ms
+import timeit as timeit
+import time as time
 
-print('cu')
-answer = ms.getch()#.decode('ASCII')
-new_answer = ms.getch()
-print(answer, 'pau', new_answer)
+
+class Timer:
+    def __init__(self) -> None:
+        self.start = time.perf_counter()
+
+    def get_time(self):
+        self.now = time.perf_counter()
+        self.time = self.now - self.start
+        return self.time
+
+t = Timer()
+while t.get_time() < 5:
+    a = ms.getch()
+    print('pau')
+
+print(a)
+
+
+
+
+
+
+
+
+#print('cu')
+#answer = ms.getch()#.decode('ASCII')
+#new_answer = ms.getch()
+#print(answer, 'pau', new_answer)
 
 
 # big brain time
