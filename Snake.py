8a83854a8f0897f8429769b94e8caf28aefc7a5d
StevenNerdWalker <lucasamvencio@gmail.com>
2022-12-21 15:31:15 -0300

    Begin Snake and Food classes

--- Snake.py
+++ Snake.py
@@ -1,6 +1,49 @@
 import msvcrt as ms
-import time as time
 import multiprocessing as mp
+import time, random
+
+
+class Snake:
+    def __init__(self, board_width, board_height) -> None:
+        """Spawn the snake in the center of the board with an initial length of two pointing to the right and with no initial direction"""
+        self.board_width = board_width
+        self.board_height = board_height
+
+        self.length = 2
+        self.x = board_width//2
+        self.y = board_height//2
+        self.head = (self.x, self.y)
+
+        self.body = [] # each element is a x, y pair , the first element is the head and the last is the tail
+        self.body.append(self.head)
+        self.tail = (self.x-1, self.y)
+        self.body.append(self.tail)
+        self.direction = None
+
+    def get_direction(self):
+        return self.direction
+
+    def change_direction(self, new_direction):
+        self.direction = new_direction
+
+    def move(self):
+        pass
+
+    def check_collision(self):
+        pass
+
+
+class Food:
+    def __init__(self) -> None:
+        pass
+
+    def spawn_food(self):
+        pass
+
+    def get_position(self):
+        pass
+
+
 
 def getch_move(q):
     # getch returns b'\x00' if its a special character, such as the arrow keys
@@ -40,3 +83,7 @@ if __name__ == '__main__':
     # então tudo fora desse if tambem seria executado pelo processo
     mp.set_start_method('spawn')
     print(get_move(3))
+
+
+
+# TODO multiplayer snake? wasd and arrows?
