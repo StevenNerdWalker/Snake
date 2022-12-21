6fa06293285f127c9392c797545e6569a3b56d9a
StevenNerdWalker <lucasamvencio@gmail.com>
2022-12-20 22:25:12 -0300

    Descobri como pegar input em qualquer instante
    caramba é um saco mexer com esse módulo, passei o dia todo tentando achar o erro
    só pra entender que o código principal tem que estar no if name == main
    vsfd

--- Snake.py
+++ Snake.py
@@ -1,33 +1,91 @@
 import msvcrt as ms
 import timeit as timeit
 import time as time
+import multiprocessing as mp
 
-
-class Timer:
-    def __init__(self) -> None:
-        self.start = time.perf_counter()
-
-    def get_time(self):
-        self.now = time.perf_counter()
-        self.time = self.now - self.start
-        return self.time
-
-t = Timer()
-while t.get_time() < 5:
+def getch_move(q):
+    print('getch_move is run by ', mp.current_process().name)
     a = ms.getch()
-    print('pau')
-
-print(a)
-
+    q.put(a)
+    return
 
 
+def get_move(stop):
+        q = mp.Queue()
+        q.put(None)
+        p = mp.Process(target=getch_move, name='Getch', args=(q, ))
+        p.start()
+
+        start = time.perf_counter()
+        while time.perf_counter() - start < stop:
+            pass
+        #print(q.get())
+        if q.qsize() != 0:
+            print(q.get())
+        p.terminate()
+        p.join()
+
+        print('get_move is run by ', mp.current_process().name)
+            
+
+if __name__ == '__main__':
+    # caralho agora eu entendi o porque do name == main
+    # é pra nenhum dos processos executar o codigo que é só do processo principal
+    # então tudo fora desse if tambem seria executado pelo processo
+    # por isso tava printando mais de uma vez
+    # passei o dia todo tentando resolver isso e agora eu entendi
+    mp.set_start_method('spawn')
+    get_move(3)
+    #print('bazinga')
+    #get_move(3)
+    print('main is run by ', mp.current_process().name)
 
 
 
 
 
-#print('cu')
-#answer = ms.getch()#.decode('ASCII')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#class Timer:
+#    def __init__(self) -> None:
+#        self.start = time.perf_counter()
+#
+#    def get_time(self):
+#        self.now = time.perf_counter()
+#        self.time = self.now - self.start
+#        return self.time
+#
+#t = Timer()
+#while t.get_time() < 5:
+#    a = ms.getch()
+#    print('pau')
+#
+#print(a)
+#
+##print('cu')
+##answer = ms.getch()#.decode('ASCII')
 #new_answer = ms.getch()
 #print(answer, 'pau', new_answer)
 
